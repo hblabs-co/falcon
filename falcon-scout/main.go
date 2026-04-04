76ace8f7ad8525fdc02d70935c9764e41d302734
Helmer Barcos <helmer@barcos.co>
2026-04-04 14:29:03 +0200

    feat: add falcon-scout main

---
+++ falcon-scout/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"hblabs.co/falcon/common/constants"
+	"hblabs.co/falcon/common/system"
+	freelancede "hblabs.co/falcon/scout/platforms/freelance.de"
+)
+
+func main() {
+
+	system.LoadEnvs()
+	system.ConfigLogger()
+	system.Init()
+	system.InitBus(system.NewBusConfig(
+		constants.StreamProjects,
+		constants.SubjectProjectCreated,
+		constants.SubjectProjectUpdated,
+	))
+
+	go freelancede.Run()
+
+	// go protalx.Run()
+	// go protaly.Run()
+	// go protalz.Run()
+	// go freelancemap.Run()
+
+	system.Wait()
+	logrus.Info("all scout platforms stopped")
+}
